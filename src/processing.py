b6d73975f19896f327b5eb4449b1fdde47f57f0c
Gerdacaezar <gerdacaezar@gmail.com>
2025-04-28 19:35:22 +0300

    внесены исправления, реммендованные наставником

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(list_of_id: list[dict], needs_state: str = 'EXECUTED') -> list[dict] | None:
-    """Функция фильтрации списка ID по 'state' (по умолчанию 'EXECUTED')"""
+    """Функция фильтрации списка ID по 'state' (по умолчанию needs_state = 'EXECUTED')"""
     result = []
     for id_dict in list_of_id:
         if id_dict['state'] == needs_state:
@@ -7,6 +7,6 @@ def filter_by_state(list_of_id: list[dict], needs_state: str = 'EXECUTED') -> li
     return result
 
 
-def sort_by_date(list_of_id: list[dict], sort_range: str = 'True') -> list[dict] | None:
+def sort_by_date(list_of_id: list[dict], sort_range: bool = True) -> list[dict] | None:
     """Функция сортировки списка ID по 'date' (по умолчанию (sort_range = True) по убыванию)"""
-    return sorted(list_of_id, key=lambda x: x['date'], reverse=True if sort_range == 'True' else False)
+    return sorted(list_of_id, key=lambda x: x['date'], reverse=sort_range)
