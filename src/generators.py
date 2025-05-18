b00f990accf39e3cf7bc0829aa41587b51bca29e
Gerdacaezar <gerdacaezar@gmail.com>
2025-05-18 17:28:47 +0300

    Завершил домашку 11.1. Прогнал проект через линтеры

--- src/generators.py
+++ src/generators.py
@@ -3,19 +3,19 @@ from typing import Generator
 
 def filter_by_currency(list_of_transactions: list[dict], currency: str) -> Generator[dict]:
     """Функция фильтрации списка транзакций по валюте операции"""
-    for x in list_of_transactions:
-        if x["operationAmount"]["currency"]["code"] == currency:
-            yield x
+    for transaction in list_of_transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            yield transaction
 
 
 def transaction_descriptions(list_of_transactions: list[dict]) -> Generator[str]:
     """Функция возвращает описание переводов из списка транзакций"""
-    for x in list_of_transactions:
-        yield x["description"]
+    for transaction in list_of_transactions:
+        yield transaction["description"]
 
 
 def card_number_generator(start: int, end: int) -> Generator[str]:
     """Функция генерирует номер карты в формате ХХХХ ХХХХ ХХХХ ХХХХ"""
-    for x in range(start, end + 1):
-        card_num = "0" * (16 - len(str(x))) + str(x)
+    for transaction in range(start, end + 1):
+        card_num = "0" * (16 - len(str(transaction))) + str(transaction)
         yield f"{card_num[:4]} {card_num[4:8]} {card_num[8:12]} {card_num[12:]}"
