8bc60958a164ce3e4c5ade35abb1a6fba552a530
Gerdacaezar <gerdacaezar@gmail.com>
2025-11-16 21:09:11 +0300

    Урок 13.1 csv и pandas. Добавил учебные материалы pandas

--- edu/csv_edu.py
+++ edu/csv_edu.py
@@ -1,18 +1,15 @@
 import csv
 
 
-rows = [['Name', 'Age', 'Gender'],
-        ['Alice', '25', 'Female'],
-        ['Bob', '30', 'Male'],
-        ['Charlie', '35', 'Male']]
+rows = [["Name", "Age", "Gender"], ["Alice", "25", "Female"], ["Bob", "30", "Male"], ["Charlie", "35", "Male"]]
 
 # Создание и запись файла csv
-with open('file.csv', 'w', newline='') as file:
+with open("file.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(rows)
 
 # Чтение файла csv
-with open('file.csv') as file:
+with open("file.csv") as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
